ce84a7f997e0d41edf0d63b723c5ed546e8b75d8
joer9514 <joer9514@gmail.com>
2021-05-20 20:11:13 -0500

    Adding task

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,10 +1,10 @@
 #ifndef SANDPILES_H
 #define SANDPILES_H
 
-#include <stdlib.h>
-#include <stdio.h>
-
+void grid_addition(int grid1[3][3], int grid2[3][3]);
+void grid_print(int grid[3][3]);
+int grid_is_ok(int grid[3][3]);
+void grid_change(int grid1[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-int validation(int grid[3][3]);
-void sandpiles_loop(int grid[3][3], int grid2[3][3]);
-#endif
+
+#endif /* SANDPILES_H */

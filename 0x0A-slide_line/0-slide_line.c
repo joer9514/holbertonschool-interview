884fe26207de1514f09fb379cda537ffd31c91b5
joer9514 <joer9514@gmail.com>
2021-07-01 23:07:31 -0500

    gc

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,70 @@
+#include "slide_line.h"
+
+/**
+ * slide_line - Prints out an array of integer, followed by a new line
+ *
+ * @line: Pointer to the array of integer to be printed
+ * @size: Number of elements in @array
+ * @direction: direction
+ *
+ * Return: 0 - EXIT_SUCCESS or 1 - EXIT_FAILURE
+ */
+int slide_line(int *line, size_t size, int direction)
+{
+
+	int i = 0, k = 0, size_n = size, data = 0;
+
+	if (line == NULL || (direction != 0 && direction != 1))
+		return (0);
+
+	if (direction == 0)
+	{
+		for (i = 0; i < size_n; i++)
+		{
+			if (line[i] == line[i + 1] && line[i] != 0)
+			{
+				line[k++] = line[i] + line[i + 1], i++;
+				line[k] = 0;
+			}
+			else if (line[i] != 0)
+			{
+				if (data == line[i])
+				{
+					line[--k] = data * 2;
+					k++;
+					data = 0;
+				}
+				else
+					line[k++] = line[i], data = line[i];
+			}
+		}
+		for (i = k; i < size_n; i++)
+			line[i] = 0;
+	}
+	else
+	{
+		k = size - 1;
+		for (i = size_n - 1; i > -1; i--)
+		{
+			if (line[i] == line[i - 1] && line[i] != 0)
+			{
+				line[k--] = line[i] + line[i - 1], i--;
+				line[k] = 0;
+			}
+			else if (line[i] != 0)
+			{
+				if (data == line[i])
+				{
+					line[++k] = data * 2;
+					k--;
+					data = 0;
+				}
+				else
+					line[k--] = line[i], data = line[i];
+			}
+		}
+		for (i = k; i > -1; i--)
+			line[i] = 0;
+	}
+	return (1);
+}
